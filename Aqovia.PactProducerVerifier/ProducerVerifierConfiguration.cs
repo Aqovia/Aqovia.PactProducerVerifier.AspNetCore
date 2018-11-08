9145e636b0464802d71d06c551552a0470882b1b
Giampaolo Paderno <giampaolo.paderno@citrus.nl>
2018-11-08 10:30:42 +0100

    Added ProviderVersion param and used it to publish verifiaction results to the Pact Broker

--- Aqovia.PactProducerVerifier/ProducerVerifierConfiguration.cs
+++ Aqovia.PactProducerVerifier/ProducerVerifierConfiguration.cs
@@ -5,11 +5,33 @@ namespace Aqovia.PactProducerVerifier.AspNetCore
 {
     public class ProducerVerifierConfiguration
     {
-        public string ProviderName { get; set; }        
+        /// <summary>
+        /// The name of the provider
+        /// </summary>
+        public string ProviderName { get; set; }
+        /// <summary>
+        /// If provider version is set the verifiaction results will be publishe to the Pact Broker
+        /// </summary>
+        public string ProviderVersion { get; set; }
+        /// <summary>
+        /// Username required to authenticate with the Pact Broker 
+        /// </summary>
         public string PactBrokerUsername { get; set; }
+        /// <summary>
+        /// Password required to authenticate with the Pact Broker
+        /// </summary>
         public string PactBrokerPassword { get; set; }
+        /// <summary>
+        /// The Pact Broker uri
+        /// </summary>
         public string PactBrokerUri { get; set; }
+        /// <summary>
+        /// Web hos builder defaulted to <c>Microsoft.AspNetCore.WebHost.CreateDefaultBuilder</c>
+        /// </summary>
         public Func<IWebHostBuilder> GetBaseWebHostBuilder { get; set; } = Microsoft.AspNetCore.WebHost.CreateDefaultBuilder;
+        /// <summary>
+        /// The type of the startup class to use when hosting the web application
+        /// </summary>
         public Type AspNetCoreStartup { get; set; }        
     }
 }
